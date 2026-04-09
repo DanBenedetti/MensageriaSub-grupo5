661ae51ba9a8193a03e3929afd7f05f11cfa3543
DanBenedetti <danilobenedetti1@gmail.com>
2026-04-09 20:15:41 -0300

    ajuste no DER

--- BD/schema.sql
+++ BD/schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS clientes (
     documento VARCHAR(20) NOT NULL
 );
 
--- Tabela de Produtos (simplificada para o projeto)
+-- Tabela de Produtos
 CREATE TABLE IF NOT EXISTS produtos (
     id INT PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
@@ -16,25 +16,24 @@ CREATE TABLE IF NOT EXISTS produtos (
 );
 
 -- Tabela de Pedidos
--- Status sugeridos pelo professor: created, paid, shipped, delivered, canceled
 CREATE TABLE IF NOT EXISTS pedidos (
     uuid VARCHAR(50) PRIMARY KEY,
     cliente_id INT REFERENCES clientes(id),
     canal VARCHAR(50),
     status VARCHAR(20) NOT NULL,
     data_criacao_payload TIMESTAMP NOT NULL,
-    data_indexacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Requisito do professor
-    vendedor_nome VARCHAR(255), -- Para simplificar a infraestrutura mínima
+    data_indexacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    vendedor_nome VARCHAR(255),
     vendedor_id INT,
     vendedor_cidade VARCHAR(100),
     vendedor_estado VARCHAR(2)
 );
 
--- Tabela de Itens do Pedido (Relacionamento N:M)
+-- Tabela de Itens do Pedido
 CREATE TABLE IF NOT EXISTS itens_pedido (
     id SERIAL PRIMARY KEY,
     pedido_uuid VARCHAR(50) REFERENCES pedidos(uuid),
     produto_id INT REFERENCES produtos(id),
     quantidade INT NOT NULL,
-    preco_unitario_no_momento DECIMAL(10, 2) NOT NULL -- Preço capturado no momento da venda
+    preco_unitario_no_momento DECIMAL(10, 2) NOT NULL
 );
